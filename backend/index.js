bc8210a5eb9c484736258298c22f0e3681209475
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-23 14:25:48 +0600

    Added a health route in backend

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,13 @@ import { connectToDB } from "./config/mongodb.js";
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({ success: true });
+});
+
 app.listen(PORT, async () => {
     console.log(`Example app listening on port ${PORT}`);
 
