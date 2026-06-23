6f57a662ede7368fb2c2ab24190224be4eb28a7a
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-23 20:51:10 +0600

    Created a property model

---
+++ backend/models/property.model.js
@@ -0,0 +1,73 @@
+import mongoose, { Schema } from "mongoose";
+
+const propertySchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        propertyType: {
+            enum: ["House", "Apartment", "Condo", "Land"],
+            default: "House"
+        },
+        status: {
+            type: String,
+            enum: ["Available", "Sold", "Pending"],
+            default: "Available"
+        },
+        images: [String],
+        price: {
+            type: Number,
+            required: true
+        },
+        area: {
+            type: Number,
+            required: true
+        },
+        beds: Number,
+        baths: Number,
+        location: {
+            lat: {
+                type: String,
+                required: true,
+            },
+            lon: {
+                type: String,
+                required: true,
+            },
+            country: {
+                type: String,
+                required: true,
+            },
+            city: {
+                type: String,
+                required: true,
+            },
+            fullAddress: {
+                type: String,
+                required: true,
+            }
+        },
+        yearBuilt: {
+            type: Number,
+            required: true
+        },
+        features: {
+            enum: ["Garage", "Balcony", "Garden", "Swimming Pool", "Storage"]
+        },
+        seller: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    },
+    { timestamps: true }
+);
+
+const Property = mongoose.model("Property", propertySchema);
+
+export default Property;
