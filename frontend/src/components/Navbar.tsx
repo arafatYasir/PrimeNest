e3fcfdb261ac43759aaf2a67b5f7f205fbe7cfd6
arafatYasir <arafat.yasir1305@gmail.com>
2026-07-01 14:19:12 +0600

    Increased navbar z-index to prevent the map from overlapping with navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export default function Navbar() {
     const [open, setOpen] = useState(false);
 
     return (
-        <header className="sticky top-0 z-50 w-full border-b border-border bg-background/80 backdrop-blur-sm supports-backdrop-filter:bg-background/50">
+        <header className="sticky top-0 z-999 w-full border-b border-border bg-background/80 backdrop-blur-sm supports-backdrop-filter:bg-background/50">
             <nav className="container mx-auto flex h-16 items-center justify-between px-4 sm:px-6 lg:px-8">
                 {/* ---- Left: Hamburger ---- */}
                 <div className="flex items-center gap-3 lg:gap-0">
