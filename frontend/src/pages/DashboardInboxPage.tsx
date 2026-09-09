638aff894f1b8723da002e13dfaef8ba7ae6f8d0
arafatYasir <arafat.yasir1305@gmail.com>
2026-09-09 20:12:40 +0600

    feat: implemented auto scrolling to the top of the page on dashboard inbox page

--- frontend/src/pages/DashboardInboxPage.tsx
+++ frontend/src/pages/DashboardInboxPage.tsx
@@ -1,6 +1,12 @@
 import DashboardInboxSidebar from "@/components/dashboard/inbox/DashboardInboxSidebar"
+import { useEffect } from "react"
 
 const DashboardInboxPage = () => {
+    // Scroll to the top of the page on first render
+    useEffect(() => {
+        window.scrollTo({ top: 0 });
+    }, []);
+
     return (
         <div className="shadow-xl">
             <DashboardInboxSidebar />
